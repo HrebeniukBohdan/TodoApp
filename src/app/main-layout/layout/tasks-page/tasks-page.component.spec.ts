cf39062db743f77074fcf43e376b57edda216610
GrebeniukBogdan <160736775+GrebeniukBogdan@users.noreply.github.com>
2024-09-25 14:59:47 +0300

    ng update @angular/core@11 @angular/cli@11

--- src/app/main-layout/layout/tasks-page/tasks-page.component.spec.ts
+++ src/app/main-layout/layout/tasks-page/tasks-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TasksPageComponent } from './tasks-page.component';
 
@@ -6,7 +6,7 @@ describe('TasksPageComponent', () => {
   let component: TasksPageComponent;
   let fixture: ComponentFixture<TasksPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TasksPageComponent ]
     })
