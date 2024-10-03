197650617d59991494296325f9961c19b3017dff
GrebeniukBogdan <160736775+GrebeniukBogdan@users.noreply.github.com>
2024-10-03 20:38:13 +0300

    unit-tests are added

--- src/app/core/guard/auth.guard.spec.ts
+++ src/app/core/guard/auth.guard.spec.ts
@@ -1,18 +1,54 @@
 import { TestBed } from '@angular/core/testing';
-
-import { AuthGuard } from './auth.guard';
+import { Router, UrlTree } from '@angular/router';
 import { AuthService } from '@core/service/auth.service';
+import { AuthGuard } from './auth.guard';
+import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    const authServiceSpy = jasmine.createSpyObj('AuthService', ['isAuthenticated']);
-    TestBed.configureTestingModule({providers: [AuthGuard, {provide: AuthService, useValue:authServiceSpy }]});
+    const authServiceSpy = jasmine.createSpyObj('AuthService', [], {
+      isAuthenticated: false
+    });
+    const routerSpy = jasmine.createSpyObj('Router', ['parseUrl']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ]
+    });
+
     guard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
+  it('should allow activation if user is authenticated', () => {
+    Object.defineProperty(authService, 'isAuthenticated', { get: () => true });
+
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    const result = guard.canActivate(route, state);
+    expect(result).toBe(true);
+  });
+
+  it('should redirect to sign-in if user is not authenticated', () => {
+    Object.defineProperty(authService, 'isAuthenticated', { get: () => false });
+
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+    const urlTree = {} as UrlTree;
+
+    router.parseUrl.and.returnValue(urlTree);
+
+    const result = guard.canActivate(route, state);
+    expect(result).toBe(urlTree);
+    expect(router.parseUrl).toHaveBeenCalledWith('/sign-in');
   });
 });
--- src/app/core/interceptor/auth.interceptor.spec.ts
+++ src/app/core/interceptor/auth.interceptor.spec.ts
@@ -1,18 +1,66 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpEvent, HttpHandler, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
 import { AuthService } from '@core/service/auth.service';
+import { of } from 'rxjs';
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthInterceptor,
-      { provide: AuthService, useValue: {} }
-    ]
-  }));
+  let interceptor: AuthInterceptor;
+  let authService: jasmine.SpyObj<AuthService>;
+  let next: jasmine.SpyObj<HttpHandler>;
 
-  it('should be created', () => {
-    const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
-    expect(interceptor).toBeTruthy();
+  beforeEach(() => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['signIn', 'signOut'], {
+      isAuthenticated: false,
+      token: null,
+    });
+
+    const nextSpy = jasmine.createSpyObj('HttpHandler', ['handle']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthInterceptor,
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: HttpHandler, useValue: nextSpy }
+      ]
+    });
+
+    interceptor = TestBed.inject(AuthInterceptor);
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    next = TestBed.inject(HttpHandler) as jasmine.SpyObj<HttpHandler>;
+  });
+
+  it('should add x-auth-token header if the user is authenticated', () => {
+    // Симулюємо автентифікацію користувача
+    Object.defineProperty(authService, 'isAuthenticated', { get: () => true });
+    Object.defineProperty(authService, 'token', { get: () => 'test-token' });
+
+    const request = new HttpRequest('GET', '/test-url');
+    
+    next.handle.and.returnValue(of({} as HttpEvent<any>));
+
+    interceptor.intercept(request, next).subscribe();
+
+    const httpRequestCaptor = next.handle.calls.mostRecent().args[0] as HttpRequest<any>;
+
+    // Перевіряємо, що заголовок було додано
+    expect(httpRequestCaptor.headers.has('x-auth-token')).toBeTrue();
+    expect(httpRequestCaptor.headers.get('x-auth-token')).toBe('test-token');
+  });
+
+  it('should not add x-auth-token header if the user is not authenticated', () => {
+    // Симулюємо неавтентифікованого користувача
+    Object.defineProperty(authService, 'isAuthenticated', { get: () => false });
+
+    const request = new HttpRequest('GET', '/test-url');
+
+    next.handle.and.returnValue(of({} as HttpEvent<any>));
+
+    interceptor.intercept(request, next).subscribe();
+
+    const httpRequestCaptor = next.handle.calls.mostRecent().args[0] as HttpRequest<any>;
+
+    // Перевіряємо, що заголовок не було додано
+    expect(httpRequestCaptor.headers.has('x-auth-token')).toBeFalse();
   });
 });
